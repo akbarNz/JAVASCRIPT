dad62e452dccae8c483cd43229eed60484b941cb
akbarNz <akbarnzeyimana15@gmail.com>
2018-09-27 11:28:25 +0200

    logiques fini

---
+++ logi.js
@@ -0,0 +1,33 @@
+var language = "en";
+var message1;
+var message2;
+var message3;
+
+if (language="en") {
+        message1 = "hello word";
+}
+
+else if (language="es") {
+    message3 = "hola mundo"
+}
+else {
+    message2 ="bonjour tout le monde";
+}
+var score = 100;
+var result;
+if (score>=90) {
+    result="A";
+}
+else if (score<=90) {
+    result="B";
+}
+else if (result<=50) {
+    result="C";
+}
+
+var femme = "femme";
+var possede = 2;
+var resuslt;
+if (possede>=2) {
+     "je possede " + femme + "s";
+}
